2cf5d6335ffa9ebb8ca22e3209ad833ccbf6b3f2
omarkhader950 <omarkhader950@gmail.com>
2023-09-19 19:41:03 +0300

    "sort packages "

--- wetherApp/app/src/main/java/com/wetherapp/ui/MainActivity.java
+++ wetherApp/app/src/main/java/com/wetherapp/ui/MainActivity.java
@@ -1,12 +1,24 @@
 package com.wetherapp.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.navigation.NavigationBarView;
+
+import com.wetherapp.R;
 import com.wetherapp.databinding.ActivityMainBinding;
 
+import java.security.PrivateKey;
+
 public class MainActivity extends AppCompatActivity {
+
+    WetherFragment wetherFragment = new WetherFragment();
+    FoodFragment foodFragment = new FoodFragment();
 
     ActivityMainBinding binding;
     @Override
@@ -15,6 +27,53 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        addFragment();
 
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if(item.getItemId() ==R.id.temp) {
+                    navigateToFragmentwether();
+                } else if (item.getItemId() ==R.id.food) {
+                    navigateToFragmentfood();
+                }
+
+                  return true ;
+
+
+
+
+                }
+
+
+
+
+            });
+        };
+
+
+    private void navigateToFragmentwether() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fram_layout,  wetherFragment);
+        fragmentTransaction.addToBackStack(null); // Optional: Add the transaction to the back stack
+        fragmentTransaction.commit();
+    }
+     private void navigateToFragmentfood() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fram_layout, foodFragment);
+        fragmentTransaction.addToBackStack(null); // Optional: Add the transaction to the back stack
+        fragmentTransaction.commit();
+    }
+     private void addFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fram_layout, wetherFragment);
+        fragmentTransaction.addToBackStack(null); // Optional: Add the transaction to the back stack
+        fragmentTransaction.commit();
     }
 }
